025a85d6faee06dfc465285fa5a2484a5b3cfcda
Umina <94283015+ViktoriaKolyasnikova@users.noreply.github.com>
2021-12-01 07:10:45 +0500

    Add files via upload

---
+++ pr-32_prakt17_1_OAP_Kolyasnikova.V/pr-32_prakt17_1_OAP_Kolyasnikova.V/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_32_prakt17_1_OAP_Kolyasnikova.V
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Размер массива: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            float[] m = new float[n];
+            Console.WriteLine("Элементы массива: ");
+            for (int i = 0; i < n; i++)
+            {
+                m[i] = float.Parse(Console.ReadLine());
+            }
+            float sum = m.Sum();
+            Console.WriteLine("Сумма: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
